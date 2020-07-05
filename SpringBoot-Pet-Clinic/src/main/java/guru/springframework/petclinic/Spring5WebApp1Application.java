7954e5bdbe3f28b51b735ed87982f3f10c8460f3
ELCOT <ELCOT@Lenovo>
2020-07-05 20:54:20 +0530

    First Commit of pet clinic

---
+++ SpringBoot-Pet-Clinic/src/main/java/guru/springframework/petclinic/Spring5WebApp1Application.java
@@ -0,0 +1,13 @@
+package guru.springframework.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WebApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5WebApp1Application.class, args);
+	}
+
+}
